f22f6d09219e69fce6869e3a81b83c5f3feee9e1
Bobby Powers <bobbypowers@gmail.com>
2020-03-15 15:51:24 -0700

    engine-core: work on figuring out the lalrpop integration

--- src/engine-core/build.rs
+++ src/engine-core/build.rs
@@ -1,4 +1,4 @@
-// Copyright 2019 The Model Authors. All rights reserved.
+// Copyright 2020 The Model Authors. All rights reserved.
 // Use of this source code is governed by the Apache License,
 // Version 2.0, that can be found in the LICENSE file.
 
@@ -6,5 +6,5 @@ extern crate lalrpop;
 
 fn main() {
     prost_build::compile_protos(&["src/ast.proto"], &["src/"]).unwrap();
-    // lalrpop::process_root().unwrap();
+    lalrpop::process_root().unwrap();
 }
