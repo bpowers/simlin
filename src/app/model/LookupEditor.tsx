12e67f3a71b4d715492e4e68074b89713de527d8
Bobby Powers <bobbypowers@gmail.com>
2020-03-28 17:02:51 -0700

    LookupEditor: live-update the chart when dragging

--- src/app/model/LookupEditor.tsx
+++ src/app/model/LookupEditor.tsx
@@ -8,9 +8,9 @@ import { List } from 'immutable';
 import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
 
-import { Table } from '../../engine/vars';
-
 import { defined } from '../common';
+import { isEqual, Point } from './drawing/common';
+import { Table } from '../../engine/vars';
 
 const styles = createStyles({});
 
@@ -26,8 +26,12 @@ interface LookupEditorPropsFull extends WithStyles<typeof styles> {
 
 // export type LookupEditorProps = Pick<LookupEditorPropsFull, 'variable' | 'viewElement' | 'data'>;
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface LookupEditorState {}
+interface LookupEditorState {
+  inDrag: boolean;
+  series: Point[];
+  yMin: number;
+  yMax: number;
+}
 
 function getAnyElementOfObject(obj: any): any | undefined {
   if (!obj) {
@@ -49,52 +53,7 @@ export const LookupEditor = withStyles(styles)(
     constructor(props: LookupEditorPropsFull) {
       super(props);
 
-      this.lookupRef = React.createRef();
-
-      this.state = {};
-    }
-
-    formatValue = (value: string | number | (string | number)[]): string | (string | number)[] => {
-      return typeof value === 'number' ? value.toFixed(3) : value;
-    };
-
-    handleMouseUp = (_e: React.MouseEvent<LineChart>) => {
-      console.log('mouse up!');
-    };
-
-    handleMouseDown = (_e: React.MouseEvent<LineChart>) => {
-      console.log('mouse down!');
-    };
-
-    handleMouseMove = (e: React.MouseEvent<LineChart>) => {
-      if (!e.hasOwnProperty('chartX') || !e.hasOwnProperty('chartY')) {
-        return;
-      }
-      const chart: any = this.lookupRef.current;
-      if (chart === null) {
-        return;
-      }
-      if (!chart.state.xAxisMap || !chart.state.yAxisMap) {
-        return;
-      }
-      const xAxisMap = getAnyElementOfObject(chart.state.xAxisMap);
-      const yAxisMap = getAnyElementOfObject(chart.state.yAxisMap);
-      const xScale = xAxisMap.scale;
-      const yScale = yAxisMap.scale;
-
-      if (!xScale || !xScale.invert || !yScale || !yScale.invert) {
-        debugger;
-        return;
-      }
-
-      const x = xScale.invert((e as any).chartX);
-      const y = yScale.invert((e as any).chartY);
-
-      console.log(`position: ${x.toFixed(3)},${y.toFixed(3)}`);
-    };
-
-    render() {
-      const { variable } = this.props;
+      const { variable } = props;
 
       let yMin = 0;
       let yMax = 0;
@@ -113,6 +72,115 @@ export const LookupEditor = withStyles(styles)(
       yMin = Math.floor(yMin);
       yMax = Math.ceil(yMax);
 
+      this.lookupRef = React.createRef();
+      this.state = {
+        inDrag: false,
+        series,
+        yMin,
+        yMax,
+      };
+    }
+
+    formatValue = (value: string | number | (string | number)[]): string | (string | number)[] => {
+      return typeof value === 'number' ? value.toFixed(3) : value;
+    };
+
+    handleContainerMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      e.stopPropagation();
+    };
+
+    handleContainerMouseUp = (e: React.MouseEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      e.stopPropagation();
+    };
+
+    handleContainerMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      e.stopPropagation();
+    };
+
+    handleContainerTouchStart = (e: React.PointerEvent<HTMLDivElement>) => {
+      const chart: any = this.lookupRef.current;
+      if (!chart) {
+        return;
+      }
+      // ensure we get 'mouse up' events (and friends) even if we leave the
+      // confines of the chart
+      (e.target as any).setPointerCapture(e.pointerId);
+    };
+
+    handleContainerTouchEnd = (_e: React.PointerEvent<HTMLDivElement>) => {};
+
+    handleContainerTouchMove = (_e: React.PointerEvent<HTMLDivElement>) => {};
+
+    handleMouseUp = () => {
+      this.setState({ inDrag: false });
+      console.log('mouse up!');
+    };
+
+    updatePoint(details: any) {
+      if (!details.hasOwnProperty('chartX') || !details.hasOwnProperty('chartY')) {
+        return;
+      }
+
+      const chart: any = this.lookupRef.current;
+      if (chart === null || !chart.state.yAxisMap) {
+        return;
+      }
+
+      const yAxisMap = getAnyElementOfObject(chart.state.yAxisMap);
+      const yScale = yAxisMap.scale;
+      if (!yScale || !yScale.invert) {
+        return;
+      }
+
+      const { yMin, yMax } = this.state;
+
+      const x = details.activePayload[0].payload.x;
+      let y = yScale.invert(details.chartY);
+      if (y > yMax) {
+        y = yMax;
+      } else if (y < yMin) {
+        y = yMin;
+      }
+
+      const series = this.state.series.map((p: Point) => {
+        if (isEqual(p.x, x)) {
+          return {
+            x,
+            y,
+          };
+        } else {
+          return p;
+        }
+      });
+
+      this.setState({ series });
+    }
+
+    handleMouseDown = (details: any) => {
+      this.setState({ inDrag: true });
+      this.updatePoint(details);
+    };
+
+    handleMouseMove = (details: any, event: React.MouseEvent<LineChart>) => {
+      if (!this.state.inDrag) {
+        return;
+      }
+
+      // if we were dragging in the chart, left the chart, stopped pressing the mouse,
+      // then moused back in we might mistakenly think we were still inDrag
+      if (event.hasOwnProperty('buttons') && event.buttons === 0) {
+        this.setState({ inDrag: false });
+      }
+
+      this.updatePoint(details);
+    };
+
+    render() {
+      const { yMin, yMax, series } = this.state;
+
       const charWidth = Math.max(yMin.toFixed(0).length, yMax.toFixed(0).length);
       const yAxisWidth = Math.max(40, 20 + charWidth * 6);
 
@@ -122,29 +190,38 @@ export const LookupEditor = withStyles(styles)(
       };
 
       return (
-        <LineChart
-          width={327}
-          height={300}
-          data={series}
-          onMouseDown={this.handleMouseDown}
-          onMouseMove={this.handleMouseMove}
-          onMouseUp={this.handleMouseUp}
-          ref={this.lookupRef}
-          layout={'horizontal'}
+        <div
+          onMouseDown={this.handleContainerMouseDown}
+          onMouseUp={this.handleContainerMouseUp}
+          onMouseMove={this.handleContainerMouseMove}
+          onPointerDown={this.handleContainerTouchStart}
+          onPointerUp={this.handleContainerTouchEnd}
+          onPointerMove={this.handleContainerTouchMove}
         >
-          <CartesianGrid horizontal={true} vertical={false} />
-          <XAxis allowDataOverflow={true} dataKey="x" domain={[left, right]} type="number" />
-          <YAxis
-            width={yAxisWidth}
-            allowDataOverflow={true}
-            domain={[yMin, yMax]}
-            type="number"
-            dataKey="y"
-            yAxisId="1"
-          />
-          <Tooltip formatter={this.formatValue} />
-          <Line yAxisId="1" type="linear" dataKey="y" stroke="#8884d8" animationDuration={300} dot={false} />
-        </LineChart>
+          <LineChart
+            width={327}
+            height={300}
+            data={series}
+            onMouseDown={this.handleMouseDown}
+            onMouseMove={this.handleMouseMove}
+            onMouseUp={this.handleMouseUp}
+            ref={this.lookupRef}
+            layout={'horizontal'}
+          >
+            <CartesianGrid horizontal={true} vertical={false} />
+            <XAxis allowDataOverflow={true} dataKey="x" domain={[left, right]} type="number" />
+            <YAxis
+              width={yAxisWidth}
+              allowDataOverflow={true}
+              domain={[yMin, yMax]}
+              type="number"
+              dataKey="y"
+              yAxisId="1"
+            />
+            <Tooltip formatter={this.formatValue} />
+            <Line yAxisId="1" type="linear" dataKey="y" stroke="#8884d8" animationDuration={300} dot={false} />
+          </LineChart>
+        </div>
       );
     }
   },
