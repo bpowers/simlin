7ed3c5991f75efcd9eda3cd3795351eda2a96cd8
Bobby Powers <bobbypowers@gmail.com>
2020-09-12 11:06:17 -0700

    lib: forbid unsafe

--- src/engine-core/src/lib.rs
+++ src/engine-core/src/lib.rs
@@ -2,6 +2,8 @@
 // Use of this source code is governed by the Apache License,
 // Version 2.0, that can be found in the LICENSE file.
 
+#![forbid(unsafe_code)]
+
 extern crate core;
 #[macro_use]
 extern crate lazy_static;
