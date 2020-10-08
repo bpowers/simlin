615955fe95d8926b513f70bc001a29e96edc3ee4
Bobby Powers <bobbypowers@gmail.com>
2020-10-07 20:50:09 -0700

    engine-core/builtin: WIP BuiltinVisitor

--- src/engine-core/src/builtin.rs
+++ src/engine-core/src/builtin.rs
@@ -2,4 +2,44 @@
 // Use of this source code is governed by the Apache License,
 // Version 2.0, that can be found in the LICENSE file.
 
-// pub struct BuiltinVisitor {}
+use crate::ast::Expr;
+use crate::common::Ident;
+use crate::xmile;
+use std::collections::HashMap;
+
+// FIXME: remove when done here
+#[allow(dead_code)]
+pub struct BuiltinVisitor<'a> {
+    variable_name: &'a str,
+    models: &'a HashMap<String, HashMap<Ident, &'a xmile::Var>>,
+    pub modules: HashMap<Ident, xmile::Var>,
+    pub vars: HashMap<Ident, xmile::Var>,
+    n: usize,
+}
+
+impl<'a> BuiltinVisitor<'a> {
+    // FIXME: remove when done here
+    #[allow(dead_code)]
+    pub fn new(
+        variable_name: &'a str,
+        _ast: &Expr,
+        models: &'a HashMap<String, HashMap<Ident, &'a xmile::Var>>,
+    ) -> Self {
+        Self {
+            variable_name,
+            models,
+            modules: Default::default(),
+            vars: Default::default(),
+            n: 0,
+        }
+    }
+}
+
+#[test]
+fn test_builtin_visitor() {
+    let _visitor = BuiltinVisitor::new(
+        "test",
+        &Expr::Const("0.0".to_string(), 0.0),
+        &HashMap::new(),
+    );
+}
