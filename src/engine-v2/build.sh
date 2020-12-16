f41c0d0c2ee1aabe25ad5c44703b6462409694b9
Bobby Powers <bobbypowers@gmail.com>
2020-12-15 22:11:36 -0800

    build: add yarn commands for building engine + importer

--- src/engine-v2/build.sh
+++ src/engine-v2/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR"
+
 # Check if jq is installed
 if ! [ -x "$(command -v jq)" ]; then
     echo "jq is not installed" >& 2
