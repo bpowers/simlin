4fa139e99890ea671b085c27fd29cc15e0bc1cb4
Bobby Powers <bobbypowers@gmail.com>
2020-12-21 09:58:26 -0800

    engine-interface: human readable names for error codes

--- src/engine-interface.ts
+++ src/engine-interface.ts
@@ -141,9 +141,114 @@ export interface Error {
   /**
    * @returns {number}
    */
-  code: number;
+  code: ErrorCode;
   /**
    * @returns {number}
    */
-  kind: number;
+  kind: ErrorKind;
+}
+
+export enum ErrorCode {
+  NoError,
+  DoesNotExist,
+  XmlDeserialization,
+  VensimConversion,
+  ProtobufDecode,
+  InvalidToken,
+  UnrecognizedEOF,
+  UnrecognizedToken,
+  ExtraToken,
+  UnclosedComment,
+  UnclosedQuotedIdent,
+  ExpectedNumber,
+  UnknownBuiltin,
+  BadBuiltinArgs,
+  EmptyEquation,
+  BadModuleInputDst,
+  BadModuleInputSrc,
+  NotSimulatable,
+  BadTable,
+  BadSimSpecs,
+  NoAbsoluteReferences,
+  CircularDependency,
+  ArraysNotImplemented,
+  MultiDimensionalArraysNotImplemented,
+  BadDimensionName,
+  BadModelName,
+  MismatchedDimensions,
+  ArrayReferenceNeedsExplicitSubscripts,
+  DuplicateVariable,
+  UnknownDependency,
+}
+
+export function errorCodeDescription(code: ErrorCode): string {
+  switch (code) {
+    case ErrorCode.NoError:
+      return 'Internal error';
+    case ErrorCode.DoesNotExist:
+      return 'Does not exist';
+    case ErrorCode.XmlDeserialization:
+      return 'XML deserialization error';
+    case ErrorCode.VensimConversion:
+      return 'Vensim conversion error';
+    case ErrorCode.ProtobufDecode:
+      return 'Internal error (protocol buffer decoding)';
+    case ErrorCode.InvalidToken:
+      return 'Invalid input in equation';
+    case ErrorCode.UnrecognizedEOF:
+      return 'Unexpectedly reached the end of the equation (mismatched parens?)';
+    case ErrorCode.UnrecognizedToken:
+      return 'Unrecognized input in equation';
+    case ErrorCode.ExtraToken:
+      return 'Extra input after equation fully parsed';
+    case ErrorCode.UnclosedComment:
+      return 'Unclosed comment';
+    case ErrorCode.UnclosedQuotedIdent:
+      return 'Unclosed quoted identifier';
+    case ErrorCode.ExpectedNumber:
+      return 'Expected a literal number';
+    case ErrorCode.UnknownBuiltin:
+      return 'Reference to unknown or unimplemented builtin';
+    case ErrorCode.BadBuiltinArgs:
+      return 'Builtin function arguments';
+    case ErrorCode.EmptyEquation:
+      return 'Variable has empty equation';
+    case ErrorCode.BadModuleInputDst:
+      return 'Module input destination is unknown';
+    case ErrorCode.BadModuleInputSrc:
+      return 'Module input source is unknown';
+    case ErrorCode.NotSimulatable:
+      return 'Model has errors and is not simulatable';
+    case ErrorCode.BadTable:
+      return 'No graphical function for specified variable';
+    case ErrorCode.BadSimSpecs:
+      return 'Simulation Specs are not valid';
+    case ErrorCode.NoAbsoluteReferences:
+      return 'Absolute references are not supported';
+    case ErrorCode.CircularDependency:
+      return 'Circular dependency';
+    case ErrorCode.ArraysNotImplemented:
+      return 'Arrays not implemented';
+    case ErrorCode.MultiDimensionalArraysNotImplemented:
+      return 'Multi-dimensional arrays not implemented';
+    case ErrorCode.BadDimensionName:
+      return 'Unknown dimension name';
+    case ErrorCode.BadModelName:
+      return 'Unknown model name';
+    case ErrorCode.MismatchedDimensions:
+      return 'Mismatched dimensions';
+    case ErrorCode.ArrayReferenceNeedsExplicitSubscripts:
+      return 'Array reference needs explicit subscripts';
+    case ErrorCode.DuplicateVariable:
+      return 'Duplicate variable';
+    case ErrorCode.UnknownDependency:
+      return 'Equation refers to unknown variable';
+  }
+}
+
+export enum ErrorKind {
+  Import,
+  Model,
+  Simulation,
+  Variable,
 }
