aac0b7e9b6d853c2cd06b5d2bffc1e998ec1eb32
Bobby Powers <bobbypowers@gmail.com>
2021-01-01 13:33:46 -0800

    importer: package up

--- src/importer/index.ts
+++ src/importer/index.ts
@@ -2,15 +2,15 @@
 // Use of this source code is governed by the Apache License,
 // Version 2.0, that can be found in the LICENSE file.
 
-let cachedWasmModule: typeof import('./pkg') | undefined;
-function getWasmModule(): Promise<typeof import('./pkg')> {
+let cachedWasmModule: typeof import('./core/importer') | undefined;
+function getWasmModule(): Promise<typeof import('./core/importer')> {
   return new Promise((resolve, reject) => {
     if (cachedWasmModule) {
       resolve(cachedWasmModule);
       return;
     }
 
-    import('./pkg')
+    import('./core/importer')
       .then((module) => {
         cachedWasmModule = module;
         resolve(module);
