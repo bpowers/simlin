913752f64c2793fc3785ae94c464d019f56b0b1c
Bobby Powers <bobbypowers@gmail.com>
2021-02-07 17:22:25 -0800

    app/NewUser: fix privacy + terms links

--- src/app/NewUser.tsx
+++ src/app/NewUser.tsx
@@ -103,11 +103,11 @@ export class NewUser extends React.Component<NewUserProps, NewUserState> {
     const termsLabel = (
       <span>
         I agree to the&nbsp;
-        <a href="/legal/" target="_blank">
+        <a href="https://simlin.com/terms" target="_blank">
           Terms and Conditions
         </a>
         &nbsp;and&nbsp;
-        <a href="/privacy/" target="_blank">
+        <a href="https://simlin.com/privacy" target="_blank">
           Privacy Policy
         </a>
         .
