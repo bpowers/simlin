3047e4111c5c0f280af66636444d494c21fff1b1
Bobby Powers <bobbypowers@gmail.com>
2021-02-25 21:25:06 -0800

    engine/units: jot some notes down

--- src/system-dynamics-engine/src/units.rs
+++ src/system-dynamics-engine/src/units.rs
@@ -1,3 +1,19 @@
 // Copyright 2021 The Model Authors. All rights reserved.
 // Use of this source code is governed by the Apache License,
 // Version 2.0, that can be found in the LICENSE file.
+
+use std::collections::BTreeMap;
+
+use crate::ast::Expr;
+
+#[allow(dead_code)]
+pub struct Context {
+    units: BTreeMap<String, Option<Expr>>,
+}
+
+#[test]
+fn test_basic_unit_checks() {
+    // from a set of datamodel::Units build a Context
+
+    // with a context, check if a set of variables unit checks
+}
