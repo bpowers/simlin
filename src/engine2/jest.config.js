65c3215218a08d765644a1f344fd56a287339846
Bobby Powers <bobbypowers@gmail.com>
2026-01-15 08:38:01 -0800

    engine2: convert jest config from TypeScript to JavaScript
    Jest requires ts-node to parse TypeScript config files. Rather than
    adding another dependency, convert the config to JavaScript which Jest
    can parse natively. The config is simple and doesn't benefit from
    TypeScript type checking.

--- src/engine2/jest.config.js
+++ src/engine2/jest.config.js
@@ -2,13 +2,12 @@
 // Use of this source code is governed by the Apache License,
 // Version 2.0, that can be found in the LICENSE file.
 
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js'],
 };
 
-export default config;
+module.exports = config;
